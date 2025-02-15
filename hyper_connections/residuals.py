f9cb0659e2cc97a2f228a184eb6890460ca8f215
lucidrains <lucidrains@gmail.com>
2025-02-15 09:24:48 -0800

    add another classic residual gating that works in practice

--- hyper_connections/residuals.py
+++ hyper_connections/residuals.py
@@ -2,7 +2,7 @@ import torch
 from torch import nn
 from torch.nn import Module
 
-from einops import rearrange
+from einops import rearrange, pack, unpack
 
 class GRUGatedResidual(Module):
     def __init__(
@@ -13,10 +13,28 @@ class GRUGatedResidual(Module):
         self.gru = nn.GRUCell(dim, dim)
 
     def forward(self, x, residual):
+        x, packed_shape = pack([x], '* d')
+        residual, _ = pack([residual], '* d')
 
-        gated_output = self.gru(
-            rearrange(x, 'b n d -> (b n) d'),
-            rearrange(residual, 'b n d -> (b n) d')
-        )
+        output = self.gru(x, residual)
 
-        return gated_output.reshape_as(x)
+        output, = unpack(output, packed_shape, '* d')
+        return output
+
+class GatedResidual(Module):
+    def __init__(
+        self,
+        dim,
+        fine_gate = False
+    ):
+        super().__init__()
+
+        self.to_learned_mix = nn.Linear(dim * 2, dim if fine_gate else 1)
+
+    def forward(self, x, residual):
+        x_and_residual, _ = pack([x, residual], 'b n *')
+
+        mix = self.to_learned_mix(x_and_residual)
+
+        out = x.lerp(residual, mix.sigmoid())
+        return out
